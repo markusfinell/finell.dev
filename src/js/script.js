247afbbafe2012d3c6e9628543bc06d1ead3aa1b
Markus Finell <markus@finell.dev>
2025-09-06 10:41:11 +0200

    article fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,5 @@
 document.documentElement.style.setProperty('--root-size', window.getComputedStyle(document.documentElement).fontSize.replace('px', ''));
+
+setTimeout(() => {
+    document.querySelector('.logo').classList.add('transition-active');
+}, 2000);
